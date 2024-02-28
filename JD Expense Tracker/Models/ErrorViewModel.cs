5ceb0eba62d4d7a653daecbae8a748cc02d7c946
Jeevan Dsouza <jeevanjd97@gmail.com>
2024-02-28 01:51:10 -0600

    Add project files.

---
+++ JD Expense Tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JD_Expense_Tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
